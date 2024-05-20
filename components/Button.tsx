3a38c91bb98cbb0d06c634527b6445589167b451
Arkadiusz Sarach <ajek1993@gmail.com>
2024-05-20 09:30:28 +0200

    add getapp component

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,17 +5,33 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green" | "btn_green" | "btn_white_text";
+  variant:
+    | "btn_dark_green"
+    | "btn_green"
+    | "btn_white_text"
+    | "btn_white"
+    | "btn_dark_green_outline";
+  full?: boolean;
 };
 
-export default function Button({ type, title, icon, variant }: ButtonProps) {
+export default function Button({
+  type,
+  title,
+  icon,
+  variant,
+  full,
+}: ButtonProps) {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${full && "w-full"}}`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      {<span className="bold-16 whitespace-nowrap">{title}</span>}
+      {
+        <span className="bold-16 cursor-pointer whitespace-nowrap">
+          {title}
+        </span>
+      }
     </button>
   );
 }
